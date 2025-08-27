097e163b3d34069174b61a0e0767ba0d36007bcb
marialerio <leriomariasheena@gmail.com>
2025-08-27 09:48:35 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClsf6xkGWH0xxdhir_A-E6nTeIMjap5Qw",
+  authDomain: "yi-goals.firebaseapp.com",
+  projectId: "yi-goals",
+  storageBucket: "yi-goals.firebasestorage.app",
+  messagingSenderId: "712223424527",
+  appId: "1:712223424527:web:9a207fb02a5746b5d2237a"
+};
+
+const app = initializeApp(firebaseConfig);
